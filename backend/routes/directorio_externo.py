f91a75beb4db63be0d0dfc37f58030cdd2c9b141
Nester Vear <diego24tinajero@gmail.com>
2025-07-15 18:07:13 -0600

    backend 1.2

--- backend/routes/directorio_externo.py
+++ backend/routes/directorio_externo.py
@@ -1,26 +1,47 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, request
 from models import DirectorioExterno
 from extensions import db
 
 directorio_ext_bp = Blueprint('directorio_ext_bp', __name__)
 
-@directorio_ext_bp.route('/api/directorio-externo', methods=['GET'])
-def obetener_directorio_externo():
-    registros = DirectorioExterno.query.all()
-    resultado = [
-        {"id": r.id, "nombre": r.nombre, "cargo": r.cargo, "institucion": r.institucion}
-        for r in registros
-    ]
-    return jsonify(resultado)
+@directorio_externo_routes_bp.route('/', methods=['GET'], endpoint='get_directorios')
+def get_directorios():
+    directorios = DirectorioExterno.query.all()
+    return jsonify([{
+        'id':d.id,
+        'nombre':d.nombre,
+        'cargo':d.cargo,
+        'institucion':d.institucion
+    }for d in directorios])
 
-@directorio_ext_bp.route('/', methods=['POST'])
-def agregar_contacto_externo():
-    data = request.json
-    nuevo_contacto = DirectorioExterno(
-        nombre=data.get("nombre"),
-        cargo=data.get("cargo"),
-        institucion=data.get("institucion")
+@directorio_externo_routes_bp.route('/', methods=['POST'], endpoint='crear_directorio')
+def crear_directorio():
+    data = request.get_json()
+    nuevo_directorio = DirectorioExterno(
+        nombre=data.get['nombre'],
+        cargo=data.get('cargo'),
+        institucion=data.get('institucion')
     )
-    db.session.add(nuevo_contacto)
+    db.session.add(nuevo_directorio)
     db.session.commit()
-    return jsonify({"mensaje": "Contacto Agregado"}), 201
+    return jsonify({"mensaje": "Contacto Agregado", 'id': nuevo_directorio.id}), 201
+
+
+@directorio_externo_routes_bp.route('/<int:id>', methods=['PUT'], endpoint='actualizar_directorio')
+def actualizar_directorio(id):
+    directorio = DirectorioExterno.query.get_or_404(id)
+    data = request.get_json()
+    for key, value in data.items():
+        setattr(directorio, key, value)
+        db.session.commit()
+        return jsonify({'mensaje': 'Directorio actualizado'})
+
+@directorio_externo_routes_bp.route('/<int:id>', methods=['DELETE'], endoint='eliminar_directorio')
+def eliminar_directorio(id):
+    directorio = DirectorioExterno.query.get_or_404(id)
+    db.session.delete(directorio)
+    db.sesion.commit()
+    return jsonify({'mensaje': 'Directorio eliminado'})
+
+# Creado por: Nester Vear 🐻
+# GitHub: github.com/NesterVear
